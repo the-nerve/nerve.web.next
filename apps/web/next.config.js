3aa1c47d3d2f07b2a365bdc184d37cbeb4da7a82
Chris Hahn <chahn@littleotterhealth.com>
2023-03-11 11:15:58 -0600

    chore: working tsconfig

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: true,
   reactStrictMode: true,
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    /**
+     * ! Dangerously allow production builds to successfully complete even if our project has errors.
+     * ! We are allowing this because we are running type checks in a separate step in our CI pipeline.
+     */
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
