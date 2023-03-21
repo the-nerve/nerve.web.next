f8fd8c4ab0ede6446ec21b461480ba96cb267a48
Chris Hahn <chahn@littleotterhealth.com>
2023-03-21 08:41:41 -0500

    chore: continue install and setup

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Provider as JotaiProvider } from 'jotai';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
@@ -7,7 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const App = ({ Component, pageProps }: AppProps) => {
   return (
     <main className={inter.className}>
-      <Component {...pageProps} />
+      <JotaiProvider>
+        <Component {...pageProps} />
+      </JotaiProvider>
     </main>
   );
 };
