54a140b01f9787558353ded8c042d7fada078647
Chris Hahn <chris+digital@chrishahn.me>
2023-06-10 07:42:14 -0500

    chore: remove mux as video host

--- apps/admin/src/core/integrations/sanity/src/schemas/video.tsx
+++ apps/admin/src/core/integrations/sanity/src/schemas/video.tsx
@@ -59,7 +59,6 @@ export const schema = defineType({
         list: [
           { title: 'Vimeo', value: 'vimeo' },
           { title: 'Youtube', value: 'youtube' },
-          { title: 'Mux', value: 'mux' },
         ],
       },
     }),
