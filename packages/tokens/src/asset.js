d5da4feea478b1cd0a53ceca62b92142b6802c38
Chris Hahn <codingwithchris@users.noreply.github.com>
2023-06-19 10:04:49 -0500

    feat: ds tokens (#40)
    start establishing design system tokens

--- packages/tokens/src/asset.js
+++ packages/tokens/src/asset.js
@@ -1,42 +1,5 @@
 module.exports = {
   asset: {
-    font: {
-      medium: {
-        name: { value: 'Covik Sans Medium' },
-        weight: {
-          value: '{font.weight.medium}',
-        },
-        style: {
-          value: 'normal',
-        },
-      },
-      'medium-italic': {
-        name: { value: 'Covik Sans Medium Italic' },
-        weight: {
-          value: '{font.weight.medium}',
-        },
-        style: {
-          value: 'italic',
-        },
-      },
-      bold: {
-        name: { value: 'Covik Sans Bold' },
-        weight: {
-          value: '{font.weight.bold}',
-        },
-        style: {
-          value: 'normal',
-        },
-      },
-      'bold-italic': {
-        name: { value: 'Covik Sans Bold Italic' },
-        weight: {
-          value: '{font.weight.bold}',
-        },
-        style: {
-          value: 'italic',
-        },
-      },
-    },
+    font: {},
   },
 };
