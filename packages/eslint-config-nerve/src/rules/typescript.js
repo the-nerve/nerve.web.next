5db94fb28f7dd8ce632a15190dcdc0a02096c782
Chris Hahn <chris+digital@chrishahn.me>
2023-07-13 08:19:28 -0500

    chore: fix rules

--- packages/eslint-config-nerve/src/rules/typescript.js
+++ packages/eslint-config-nerve/src/rules/typescript.js
@@ -11,6 +11,9 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'error',
     '@typescript-eslint/no-unused-vars': 'error',
 
+    // Any can sometimes be helpful when you don't know the type or want to move fast
+    '@typescript-eslint/no-explicit-any': 'off',
+
     // disable rules that are already checked by TS
     'consistent-return': 'off',
     'no-use-before-define': 'off',
