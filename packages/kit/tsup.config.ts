fd5b2aba06f2dea0e4e60679e1f631179bbdfcd5
Chris Hahn <chris+digital@chrishahn.me>
2024-02-24 10:25:40 -0600

    chore: clean up build

--- packages/kit/tsup.config.ts
+++ packages/kit/tsup.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: {
-    devices: 'src/devices/index.ts',
     env: 'src/env/index.ts',
-    next: 'src/next/index.ts',
     node: 'src/node/index.ts',
     react: 'src/react/index.ts',
-    testing: 'src/testing/index.ts',
     utils: 'src/utils/index.ts',
   },
   external: ['vitest', 'react', 'react-dom'],
